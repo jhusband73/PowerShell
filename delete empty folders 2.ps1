c1da50f31806f5361571bc3513a4fb1d098e560f
jhusband73 <jeff.husband@gmail.com>
2024-09-23 11:28:13 -0400

    added file clean up and cluster scripts

---
+++ delete empty folders 2.ps1
@@ -0,0 +1,11 @@
+﻿
+$LogPath = "d:\jeff's\my music" # replace this with your root path...
+Get-ChildItem -LiteralPath $LogPath -Recurse | Where-Object {
+    $_.PSIsContainer -and `
+    @(Get-ChildItem -LiteralPath $_.Fullname -Recurse | Where { -not $_.PSIsContainer }).Count -eq 0 } |
+    Remove-Item -Recurse -force -WhatIf
+
+# get-childitem "d:\jeff's\my music" -include *.jpg -recurse -hidden -force | foreach ($_) {remove-item $_.fullname}
+#get-childitem "d:\jeff's\my music" -include *.dat -recurse -hidden | foreach ($_) {remove-item $_.fullname -Force}
+
+#get-childitem "d:\jeff's\my music" -include *.mp3 -recurse  | foreach ($_) {move-item -Path "d:\Jeff's\my music"}
---
+++ delete empty folders1.ps1
@@ -0,0 +1,16 @@
+﻿$items = Get-ChildItem -Recurse
+# delete empty folders
+
+foreach($item in $items)
+{
+      if( $item.PSIsContainer )
+      {
+            $subitems = Get-ChildItem -Recurse -Path $item.FullName
+            if($subitems -eq $null)
+            {
+                  "Remove item: " + $item.FullName
+                  Remove-Item $item.FullName
+            }
+            $subitems = $null
+      }
+}
